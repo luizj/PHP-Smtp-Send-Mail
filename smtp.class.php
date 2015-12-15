4a0de7aa39f8edb8d4a152be98b599f70d814f72
luizj <lngj2003@gmail.com>
2015-12-15 16:17:55 -0200

    Update smtp.class.php

--- smtp.class.php
+++ smtp.class.php
@@ -26,27 +26,24 @@ class Smtp
         if(!$this->wRecv("220"))return false; //INIT
         $this->Put("EHLO <".$this->host.">");
         if(!$this->wRecv("250"))return false; //HELLO
-        
+
         if($this->use_tls)$this->startTLS();
-        
+
         if($this->auth)
         {
             $this->Put("AUTH LOGIN");
             if(!$this->wRecv("334"))return false; // ok
-
             $this->Put(base64_encode($this->user));
             if(!$this->wRecv("334"))return false; // ok
-
             $this->Put(base64_encode($this->pass));
             if(!$this->wRecv("235"))return false; // authenticated
         }
-        
+
         $this->Put("MAIL FROM: <".$this->from.">");
         if(!$this->wRecv("250"))return false; // ok
-
         $this->Put("RCPT TO: <".$to.">");
         if(!$this->wRecv("250"))return false; // ok
-        
+
         $this->Put("DATA");
         $this->Put($this->toHeader($to, $subject));
         $this->Put($msg);
@@ -59,9 +56,8 @@ class Smtp
     function startTLS()
     {
         if (!$this->TLS)return false;
-	$this->Put("STARTTLS");
-	if(!$this->wRecv("220"))return false; //HELLO
-
+        $this->Put("STARTTLS");
+        if(!$this->wRecv("220"))return false; //HELLO
         // Begin encrypted connection
         if (!stream_socket_enable_crypto(
             $this->conn,
@@ -72,7 +68,7 @@ class Smtp
         }
         return true;
     }
-    
+
     function toHeader($to, $subject)
     {
         $header = "Message-Id: <". date('YmdHis').".". md5(microtime()). strrchr($this->from,'@') ."> \r\n";
@@ -81,11 +77,11 @@ class Smtp
         $header .= "Subject: ".$subject."\r\n";
         $header .= "Date: ". date('D, d M Y H:i:s O') ."\r\n";
         $header .= "MIME-Version: 1.0\r\n";
-	$header .= "X-Mailer: PHPMail\r\n";
+        $header .= "X-Mailer: PHPMail\r\n";
         $header .= "Content-Type: Text/HTML; charset=UTF-8\r\n";
         return $header;
     }
-    
+
     function Put($value)
     {
         if($this->debug)
@@ -93,7 +89,6 @@ class Smtp
             echo "-> ".$value."\x0D\x0A";
         }
         return fputs($this->conn, $value."\r\n");
-
     }
 
     function wRecv($cod)
@@ -106,14 +101,14 @@ class Smtp
             if(substr($c,0, 3) == $cod)
             {
                 if($cod == "250"){//Addons
-			if(substr($c,4, 10) == "AUTH LOGIN")$this->auth = true;
-			if(substr($c,4, 8)  == "STARTTLS")  $this->TLS  = true;
-		}
-		if($cod == "220"){//EHLO
-			$exp = explode(" ", $c);
-			$this->host = trim($exp[1]);
-		}
-		$ret = true;
+                if(substr($c,4, 10) == "AUTH LOGIN")$this->auth = true;
+                if(substr($c,4, 8)  == "STARTTLS")  $this->TLS  = true;
+            }
+            if($cod == "220"){//EHLO
+                $exp = explode(" ", $c);
+                $this->host = trim($exp[1]);
+            }
+            $ret = true;
             }
             if(substr($c,3, 1) != "-")return $ret;
         }
